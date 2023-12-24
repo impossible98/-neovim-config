cd8381a86014ece585838994b5d465c37cd8e4aa
impossible98 <christoleavenow@outlook.com>
2023-12-25 06:45:10 +0800

    fix: update options.lua

--- templates/lua/options.lua
+++ templates/lua/options.lua
@@ -1,8 +1,8 @@
-local set = vim.opt
+local opt = vim.opt
 
 -- Highlight the text line of the cursor with CursorLine hl-CursorLine.
-set.cursorline = true
+opt.cursorline = true
 -- Same as :print, but precede each line with its line number.
-set.number = true
+opt.number = true
 -- Enables 24-bit RGB color in the TUI.
-set.termguicolors = true
+opt.termguicolors = true
